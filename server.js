cc28ee59183474b30ecf1e62e588370deaaede9a
Ibrahim Lahham <abo.la7am.10@gmail.com>
2021-01-22 10:13:50 +0200

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 app.use(express.static('public'));
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port,function() {
 console.log('listening',port);
 })
